35611b0910c3b35f82637d5cd936dc269c9e27e8
prushton2 <peterrushton418@gmail.com>
2024-02-27 22:53:23 -0500

    Parser optimizations

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,17 @@
 use crate::tokenizer::{TokenType, Token};
 
 pub fn parse(tokens_imm: &Vec<Token>) -> Result<Token, String> {
-    let mut tokens = tokens_imm.clone();
-    
-    let mut prev_len = tokens.len();
+    let mut tokens: Vec<Token> = tokens_imm.clone();
+    let mut prev_len: usize = tokens.len();
 
     while tokens.len() > 1 {
-        insert_mult(&mut tokens);
-        parse_parens(&mut tokens);
-        parse_exponents(&mut tokens);
-        parse_mult(&mut tokens);
-        parse_plus(&mut tokens);
+        for i in 0..tokens.len()-2 {
+            insert_mult(&mut tokens, i);
+            parse_parens(&mut tokens, i);
+            parse_exponents(&mut tokens, i);
+            parse_mult(&mut tokens, i);
+            parse_plus(&mut tokens, i);
+        }
         
         if prev_len == tokens.len() {  //if the length of the previous and current vector are the
                                        //same, nothing has happened. This means nothing will ever
@@ -25,95 +26,88 @@ pub fn parse(tokens_imm: &Vec<Token>) -> Result<Token, String> {
 }
 
 
-fn parse_parens(tokens: &mut Vec<Token>) {
-    for i in 0..tokens.len()-1 {
-        if  tokens[i].token == TokenType::OpenParen &&
-            tokens[i+1].token == TokenType::Number && 
-            tokens[i+2].token == TokenType::CloseParen {
-             
-            tokens.remove(i+2);
-            tokens.remove(i);
-            return;
-        }
-    } 
+fn parse_parens(tokens: &mut Vec<Token>, i: usize) {
+    if tokens.len() < 3 {return;}
+    if  tokens[i].token == TokenType::OpenParen &&
+        tokens[i+1].token == TokenType::Number && 
+        tokens[i+2].token == TokenType::CloseParen {
+         
+        tokens.remove(i+2);
+        tokens.remove(i);
+    }
 }
 
-fn insert_mult(tokens: &mut Vec<Token>) {
-    for i in 0..tokens.len()-1 {
-        if tokens[i].token == TokenType::Number &&
-           tokens[i+1].token == TokenType::Number {
-            
-            tokens.insert(i+1, Token{token: TokenType::Multiply, lexeme: String::from("*")});
-            return;
-        }
+fn insert_mult(tokens: &mut Vec<Token>, i: usize) {
+    if tokens.len() < 3 {return;}
+    if tokens[i].token == TokenType::Number &&
+       tokens[i+1].token == TokenType::Number {
+        
+        tokens.insert(i+1, Token{token: TokenType::Multiply, lexeme: String::from("*")});
     }
 
 }
 
-fn parse_exponents(tokens: &mut Vec<Token>) {
-    for i in 0..tokens.len()-1 {
-        if  tokens[i].token == TokenType::Number &&
-            tokens[i+1].token == TokenType::Exponent && 
-            tokens[i+2].token == TokenType::Number {
-            
+fn parse_exponents(tokens: &mut Vec<Token>, i: usize) {
+    if tokens.len() < 3 {return;}
+    if  tokens[i].token == TokenType::Number &&
+        tokens[i+1].token == TokenType::Exponent && 
+        tokens[i+2].token == TokenType::Number {
+        
 
-            let base = tokens[i].lexeme.parse::<f32>().unwrap();
-            let exponent = tokens[i+2].lexeme.parse::<f32>().unwrap();
+        let base = tokens[i].lexeme.parse::<f32>().unwrap();
+        let exponent = tokens[i+2].lexeme.parse::<f32>().unwrap();
 
-            let power = base.powf(exponent);
+        let power = base.powf(exponent);
 
-            tokens.remove(i+2);
-            tokens.remove(i+1);
-            tokens[i].lexeme = power.to_string();
-            return;
-        }
-    } 
+        tokens.remove(i+2);
+        tokens.remove(i+1);
+        tokens[i].lexeme = power.to_string();
+        return;
+    }
 }
 
-fn parse_mult(tokens: &mut Vec<Token>) {
-    for i in 0..tokens.len()-1 {
-        if  tokens[i].token == TokenType::Number &&
-            (tokens[i+1].token == TokenType::Multiply || tokens[i+1].token == TokenType::Divide) && 
-            tokens[i+2].token == TokenType::Number {
-            
+fn parse_mult(tokens: &mut Vec<Token>, i: usize) {
+    if tokens.len() < 3 {return;}
+    if  tokens[i].token == TokenType::Number &&
+        (tokens[i+1].token == TokenType::Multiply || tokens[i+1].token == TokenType::Divide) && 
+        tokens[i+2].token == TokenType::Number {
+        
 
-            let operand1 = tokens[i].lexeme.parse::<f32>().unwrap();
-            let operand2 = tokens[i+2].lexeme.parse::<f32>().unwrap();
+        let operand1 = tokens[i].lexeme.parse::<f32>().unwrap();
+        let operand2 = tokens[i+2].lexeme.parse::<f32>().unwrap();
 
-            let product = match tokens[i+1].token {
-                TokenType::Multiply => operand1 * operand2,
-                TokenType::Divide => operand1 / operand2,
-                _ => 0.0,
-            };
+        let product = match tokens[i+1].token {
+            TokenType::Multiply => operand1 * operand2,
+            TokenType::Divide => operand1 / operand2,
+            _ => 0.0,
+        };
 
-            tokens.remove(i+2);
-            tokens.remove(i+1);
-            tokens[i].lexeme = product.to_string();
-            return;
-        }
-    } 
+        tokens.remove(i+2);
+        tokens.remove(i+1);
+        tokens[i].lexeme = product.to_string();
+        return;
+    }
 }
 
-fn parse_plus(tokens: &mut Vec<Token>) {
-    for i in 0..tokens.len()-1 {
-        if  tokens[i].token == TokenType::Number &&
-           (tokens[i+1].token == TokenType::Plus || tokens[i+1].token == TokenType::Minus) && 
-            tokens[i+2].token == TokenType::Number {
-            
+fn parse_plus(tokens: &mut Vec<Token>, i: usize) {
+    if tokens.len() < 3 {return;}
+    if  tokens[i].token == TokenType::Number &&
+       (tokens[i+1].token == TokenType::Plus || tokens[i+1].token == TokenType::Minus) && 
+        tokens[i+2].token == TokenType::Number {
+        
 
-            let operand1 = tokens[i].lexeme.parse::<f32>().unwrap();
-            let operand2 = tokens[i+2].lexeme.parse::<f32>().unwrap();
+        let operand1 = tokens[i].lexeme.parse::<f32>().unwrap();
+        let operand2 = tokens[i+2].lexeme.parse::<f32>().unwrap();
 
-            let sum = match tokens[i+1].token {
-                TokenType::Plus => operand1 + operand2,
-                TokenType::Minus => operand1 - operand2,
-                _ => 0.0,
-            };
+        let sum = match tokens[i+1].token {
+            TokenType::Plus => operand1 + operand2,
+            TokenType::Minus => operand1 - operand2,
+            _ => 0.0,
+        };
 
-            tokens.remove(i+2);
-            tokens.remove(i+1);
-            tokens[i].lexeme = sum.to_string();
-            return;
-        }
-    } 
+        tokens.remove(i+2);
+        tokens.remove(i+1);
+        tokens[i].lexeme = sum.to_string();
+        return;
+    }
 }
