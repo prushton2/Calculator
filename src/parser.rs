09f1fb583f470e5beedd3299dc29b00d401caecf
prushton2 <peterrushton418@gmail.com>
2024-02-27 23:38:04 -0500

    ill make an AST

--- src/parser.rs
+++ src/parser.rs
@@ -3,22 +3,39 @@ use crate::tokenizer::{TokenType, Token};
 pub fn parse(tokens_imm: &Vec<Token>) -> Result<Token, String> {
     let mut tokens: Vec<Token> = tokens_imm.clone();
     let mut prev_len: usize = tokens.len();
+    
+    //This is a war crime, however it is the easiest way to prevent oob indexes
+    tokens.push( Token {token: TokenType::Error, lexeme: String::from("")});
+    tokens.push( Token {token: TokenType::Error, lexeme: String::from("")});
 
-    while tokens.len() > 1 {
-        for i in 0..tokens.len()-2 {
+
+    while tokens.len() > 3 {
+        for i in 0..tokens.len() {
+            
+            println!("---i:{} | {} -2= {}\n---Tokens:",i, tokens.len(), tokens.len()-2);
+            
+            for j in &tokens {
+                print!("{}", j.lexeme);
+            }
+            println!("---Functions:");
+            
+            //println!("insert_mult");
             insert_mult(&mut tokens, i);
+            if i > tokens.len()-2 {break; }
+           // println!("parse_parens {}", i);
             parse_parens(&mut tokens, i);
+            if i > tokens.len()-2 {break; }
+          //  println!("parse_exponents");
             parse_exponents(&mut tokens, i);
+            if i > tokens.len()-2 {break; }
+           // println!("parse_mult");
             parse_mult(&mut tokens, i);
+            if i > tokens.len()-2 {break; }
+           // println!("parse_add");
             parse_plus(&mut tokens, i);
+            if i > tokens.len()-2 {break; }
         }
         
-        if prev_len == tokens.len() {  //if the length of the previous and current vector are the
-                                       //same, nothing has happened. This means nothing will ever
-                                       //happen and therefore an infinite loop
-            return Err("Loop Detected".to_string());
-        }   
-        prev_len = tokens.len();
     }
 
 
@@ -27,20 +44,20 @@ pub fn parse(tokens_imm: &Vec<Token>) -> Result<Token, String> {
 
 
 fn parse_parens(tokens: &mut Vec<Token>, i: usize) {
-    if tokens.len() < 3 {return;}
     if  tokens[i].token == TokenType::OpenParen &&
         tokens[i+1].token == TokenType::Number && 
         tokens[i+2].token == TokenType::CloseParen {
          
+
         tokens.remove(i+2);
         tokens.remove(i);
     }
 }
 
 fn insert_mult(tokens: &mut Vec<Token>, i: usize) {
-    if tokens.len() < 3 {return;}
     if tokens[i].token == TokenType::Number &&
-       tokens[i+1].token == TokenType::Number {
+       tokens[i+1].token == TokenType::OpenParen &&
+       tokens[i+2].token == TokenType::Number {
         
         tokens.insert(i+1, Token{token: TokenType::Multiply, lexeme: String::from("*")});
     }
@@ -48,12 +65,10 @@ fn insert_mult(tokens: &mut Vec<Token>, i: usize) {
 }
 
 fn parse_exponents(tokens: &mut Vec<Token>, i: usize) {
-    if tokens.len() < 3 {return;}
     if  tokens[i].token == TokenType::Number &&
         tokens[i+1].token == TokenType::Exponent && 
         tokens[i+2].token == TokenType::Number {
         
-
         let base = tokens[i].lexeme.parse::<f32>().unwrap();
         let exponent = tokens[i+2].lexeme.parse::<f32>().unwrap();
 
@@ -62,12 +77,10 @@ fn parse_exponents(tokens: &mut Vec<Token>, i: usize) {
         tokens.remove(i+2);
         tokens.remove(i+1);
         tokens[i].lexeme = power.to_string();
-        return;
     }
 }
 
 fn parse_mult(tokens: &mut Vec<Token>, i: usize) {
-    if tokens.len() < 3 {return;}
     if  tokens[i].token == TokenType::Number &&
         (tokens[i+1].token == TokenType::Multiply || tokens[i+1].token == TokenType::Divide) && 
         tokens[i+2].token == TokenType::Number {
@@ -85,12 +98,10 @@ fn parse_mult(tokens: &mut Vec<Token>, i: usize) {
         tokens.remove(i+2);
         tokens.remove(i+1);
         tokens[i].lexeme = product.to_string();
-        return;
     }
 }
 
 fn parse_plus(tokens: &mut Vec<Token>, i: usize) {
-    if tokens.len() < 3 {return;}
     if  tokens[i].token == TokenType::Number &&
        (tokens[i+1].token == TokenType::Plus || tokens[i+1].token == TokenType::Minus) && 
         tokens[i+2].token == TokenType::Number {
@@ -108,6 +119,5 @@ fn parse_plus(tokens: &mut Vec<Token>, i: usize) {
         tokens.remove(i+2);
         tokens.remove(i+1);
         tokens[i].lexeme = sum.to_string();
-        return;
     }
 }
